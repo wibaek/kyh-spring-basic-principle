3ad68101868f3172e0ae169460c48e3996ffa961
Wibaek Park <devmuromi@gmail.com>
2025-05-31 15:51:04 +0900

    12. 회원 도메인 개발

---
+++ core/src/main/java/com/wibaek/core/member/Grade.java
@@ -0,0 +1,6 @@
+package com.wibaek.core.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ core/src/main/java/com/wibaek/core/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.wibaek.core.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
