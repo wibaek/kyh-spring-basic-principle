374bddb593d606b5c4ad2f39ad3193cac73e7994
Wibaek Park <devmuromi@gmail.com>
2025-06-01 14:55:21 +0900

    25. 스프링으로 전환하기

--- core/src/main/java/com/wibaek/core/AppConfig.java
+++ core/src/main/java/com/wibaek/core/AppConfig.java
@@ -8,21 +8,28 @@ import com.wibaek.core.member.MemberServiceImpl;
 import com.wibaek.core.member.MemoryMemberRepository;
 import com.wibaek.core.order.OrderService;
 import com.wibaek.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- core/src/main/java/com/wibaek/core/OrderApp.java
+++ core/src/main/java/com/wibaek/core/OrderApp.java
@@ -5,13 +5,15 @@ import com.wibaek.core.member.Member;
 import com.wibaek.core.member.MemberService;
 import com.wibaek.core.order.Order;
 import com.wibaek.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
