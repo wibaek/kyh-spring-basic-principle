77517490be21be0b7598805376e4911aecdc8fe9
Wibaek Park <devmuromi@gmail.com>
2025-06-01 13:50:23 +0900

    18. 새로운 할인 정책 적용과 문제점

--- core/src/main/java/com/wibaek/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/wibaek/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.wibaek.core.order;
 
 import com.wibaek.core.discount.DiscountPolicy;
-import com.wibaek.core.discount.FixDiscountPolicy;
 import com.wibaek.core.member.Member;
 import com.wibaek.core.member.MemberRepository;
 import com.wibaek.core.member.MemoryMemberRepository;
@@ -9,7 +8,7 @@ import com.wibaek.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
